61c861946ef76d3b62cd8a0da5f1380ce29cd9e5
Wladimir Teixeira Neto <wladimirtn@algartech.com>
2020-02-26 14:09:41 -0300

    Aula 11

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -4,12 +4,24 @@ import React from 'react';
 class App extends React.Component{
   
   state = {
-    nome : 'Wlad Neto'
+    nome : 'Digite seu nome'
+  }
+
+  modificarNome = (event) => {
+    let newNome = event.target.value;
+    this.setState({
+      nome: newNome
+    })
   }
 
   render(){
     return(
-    <h1> Hello {this.state.nome} Class </h1>
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} /> 
+
+        <h1> Hello {this.state.nome} Class </h1>
+
+      </React.Fragment>
     )
   }
 }
