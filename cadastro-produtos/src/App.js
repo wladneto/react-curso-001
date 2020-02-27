5d1d5b32eb14fbb1fb5b0951ef9df9243323d85d
Wladimir Teixeira Neto <wladimirtn@algartech.com>
2020-02-26 21:08:52 -0300

    Aula 19

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="alert alert-dismissible alert-warning">
+      <button type="button" className="close" data-dismiss="alert">&times;</button>
+      <h4 className="alert-heading">Warning!</h4>
+      <p className="mb-0">Hello <a href="#" className="alert-link">Wlad</a>.</p>
     </div>
   );
 }
