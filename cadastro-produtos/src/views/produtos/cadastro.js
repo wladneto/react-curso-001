afe4205a6d71a1535390c773471d43d0707a2600
Wladimir Teixeira Neto <wladimirtn@algartech.com>
2020-02-26 21:32:02 -0300

    Aula 21

---
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class CadastroProduto extends React.Component {
+    render(){
+        return(
+            <div className="card">    
+                <div className="card-header">
+                    Casdastro de Produto
+                </div>
+                <div className="card-body">
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default CadastroProduto;
