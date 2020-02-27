706684abed26c41a33815020fc906b7271fe6830
Wladimir Teixeira Neto <wladimirtn@algartech.com>
2020-02-26 21:52:45 -0300

    Aula 24

--- cadastro-produtos/src/views/produtos/cadastro.js
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -8,6 +8,52 @@ class CadastroProduto extends React.Component {
                     Casdastro de Produto
                 </div>
                 <div className="card-body">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Nome: *</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                        <div className="form-group">
+                                <label>SKU: *</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label>Descrição: *</label>
+                                <textarea className="form-control"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Preço: *</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                        <div className="form-group">
+                                <label>Fornecedor: *</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-1">
+                            <button className="btn btn-success">Salvar</button>
+                        </div>
+                        <div className="col-md-1">
+                            <button className="btn btn-primary">Limpar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
